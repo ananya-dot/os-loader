4b1748f8ec4fefc50b0d09d2b46186e785440fcb
Ananya Sachdev <83594973+ananya-dot@users.noreply.github.com>
2023-08-21 20:14:49 +0530

    a cleaner version -- lowkey works
    works for ./loader ./fib
    seg fault for ./fib

--- loader.c
+++ loader.c
@@ -3,128 +3,87 @@
 Elf32_Ehdr *ehdr;
 int fd;
 
-/*
- * Release memory and other cleanups
- */
 void loader_cleanup() {
     free(ehdr);
 }
 
-/*
- * Load and run the ELF executable file
- */
 void load_and_run_elf(char** exe) {
-    fd = open(*exe, O_RDONLY);
+    fd = open(exe[1], O_RDONLY);
+    
     if (fd < 0) {
         perror("open");
         return;
     }
-
+    
     ehdr = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
     if (ehdr == NULL) {
         perror("malloc");
         close(fd);
         return;
     }
+    
 
-    // Read ELF header
     if (read(fd, ehdr, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)) {
         perror("read");
-        free(ehdr);  // Free allocated memory on error
+        free(ehdr);
         close(fd);
         return;
     }
 
-    // Find the loadable segment containing the entry point
-    Elf32_Addr adjusted_entry = ehdr->e_entry;  // Initialize with the default entry point
+    Elf32_Addr entry = ehdr->e_entry;
+    
+    
     for (int i = 0; i < ehdr->e_phnum; ++i) {
         Elf32_Phdr phdr;
+  
+        
         lseek(fd, ehdr->e_phoff + i * ehdr->e_phentsize, SEEK_SET);
         if (read(fd, &phdr, sizeof(Elf32_Phdr)) != sizeof(Elf32_Phdr)) {
             perror("read");
-            free(ehdr);  // Free allocated memory on error
+            free(ehdr);
             close(fd);
             return;
         }
-
+        // printf("hi");
         if (phdr.p_type == PT_LOAD) {
-            if (ehdr->e_entry >= phdr.p_vaddr && ehdr->e_entry < phdr.p_vaddr + phdr.p_memsz) {
-                // Entry point is within this segment
-                adjusted_entry = ehdr->e_entry - phdr.p_vaddr + phdr.p_offset;
+            void *segment_addr = mmap(
+                NULL,
+                phdr.p_memsz,
+                PROT_READ | PROT_WRITE | PROT_EXEC,
+                MAP_ANONYMOUS | MAP_PRIVATE,
+                -1,
+                0
+            );
+
+            if (segment_addr == MAP_FAILED) {
+                perror("mmap");
+                free(ehdr);
+                close(fd);
+                return;
+            }
+
+            lseek(fd, phdr.p_offset, SEEK_SET);
+            if (read(fd, segment_addr, phdr.p_filesz) != phdr.p_filesz) {
+                perror("read");
+                free(ehdr);
+                close(fd);
+                return;
+            }
+
+            if (entry >= phdr.p_vaddr && entry < (phdr.p_vaddr + phdr.p_memsz)) {
+                entry = (entry - phdr.p_vaddr) + (Elf32_Addr)segment_addr;
+                int (*_start)() = (int (*)())entry;
+                // _start();
+                printf("entry: %d", _start());
+                // printf("User _start return value = %d\n",8);
                 break;
             }
         }
     }
-
-    // Load the segment and adjust the entry point
-    for (int i = 0; i < ehdr->e_phnum; ++i) {
-        Elf32_Phdr phdr;
-        lseek(fd, ehdr->e_phoff + i * ehdr->e_phentsize, SEEK_SET);
-        if (read(fd, &phdr, sizeof(Elf32_Phdr)) != sizeof(Elf32_Phdr)) {
-            perror("read");
-            free(ehdr);  // Free allocated memory on error
-            close(fd);
-            return;
-        }
-
-        // if (phdr.p_type == PT_LOAD) {
-            // void *segment_addr = mmap(
-            //     NULL,
-            //     phdr.p_memsz,
-            //     PROT_READ|PROT_WRITE|PROT_EXEC,
-            //     MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED,
-            //     0,
-            //     0
-            // );
-            if (phdr.p_type == PT_LOAD) {
-    void *segment_addr = mmap(
-        (void *)phdr.p_vaddr,  // Use p_vaddr as the desired virtual address
-        phdr.p_memsz,
-        PROT_READ | PROT_WRITE | PROT_EXEC,
-        MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED,
-        -1,
-        0
-    );
-
-    // if (segment_addr == MAP_FAILED) {
-    //     perror("mmap");
-    //     free(ehdr);  // Free allocated memory on error
-    //     close(fd);
-    //     return;
-    // }
-
-    lseek(fd, phdr.p_offset, SEEK_SET);
-    if (read(fd, segment_addr, phdr.p_filesz) != phdr.p_filesz) {
-        perror("read");
-        free(ehdr);  // Free allocated memory on error
-        close(fd);
-        return;
-    }
-}
-
-            
-            // if (segment_addr == MAP_FAILED) {
-            //     perror("mmap");
-            //     free(ehdr);  // Free allocated memory on error
-            //     close(fd);
-            //     return;
-            // }
-
-            // lseek(fd, phdr.p_offset, SEEK_SET);
-            // if (read(fd, segment_addr, phdr.p_filesz) != phdr.p_filesz) {
-            //     perror("read");
-            //     free(ehdr);  // Free allocated memory on error
-            //     close(fd);
-            //     return;
-            }
-        
-    
-
-    // Start execution at adjusted entry point
-
-
-    // Clean up: Free allocated memory and close file descriptor
-    loader_cleanup();
+     
+  
+  
+    // loader_cleanup();
     close(fd);
 }
 
@@ -134,8 +93,6 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    // 1. Carry out necessary checks on the input ELF file (not shown)
-    // 2. Passing it to the loader for carrying out the loading/execution
     load_and_run_elf(argv);
     return 0;
 }
